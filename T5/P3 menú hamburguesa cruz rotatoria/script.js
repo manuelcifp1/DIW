687d91507d0be79b195182ae16e8db65d3143479
manuelcifp1 <alba_rios_manuel@hotmail.com>
2024-12-15 10:14:24 +0100

    comentario en el archivo script

--- T5/P3 menú hamburguesa cruz rotatoria/script.js
+++ T5/P3 menú hamburguesa cruz rotatoria/script.js
@@ -1,3 +1,13 @@
+/*El profesor nos dice de vez en cuando que podemos añadir algo
+de javascript a nuestros proyectos y me ha parecido que este era
+un buen momento para hacerlo. 
+He creado un escuchador de eventos, en este caso el evento
+"scroll". Este evento dispara una función que hace que al desplazarse
+ el usuario 76px por el eje Y, el texto del título principal cambie
+  para ser el del título secundario y además desaparezca el título
+   secundario. Al volver al comienzo de la página, todo recupera sus
+    estilos originales.*/
+
 document.addEventListener('scroll', () => {
 
     const titulo = document.getElementById("titulo");
